0b70114f05e5274c9ecb168e6bc8ec47a35167cc
Sam Zhao <zhao.shengjun@gmail.com>
2020-01-03 17:23:13 +1000

    add sum of Jan

--- src/bankStatementAnalyzer.ts
+++ src/bankStatementAnalyzer.ts
@@ -19,6 +19,26 @@ export class BankTransactionAnalyzerSimple {
 
 		console.log(`The total of all transaction is ${totals}`);
 	}
+
+	sumOfJanuary() {
+		const data = fs.readFileSync(
+			path.resolve(BankTransactionAnalyzerSimple._RESOURCES),
+			"utf-8"
+		);
+		const lines = data.split(/\r?\n/);
+		let totals = 0;
+		for (const line of lines) {
+			const columns = line.split(",");
+			const [day, month, year] = columns[0].split("-");
+			if (month === "01") {
+				const amount = parseFloat(columns[1]);
+				totals += amount;
+			}
+		}
+
+		console.log(`The total of all transaction in January is ${totals}`);
+	}
 }
 let anaylzer = new BankTransactionAnalyzerSimple();
 anaylzer.total();
+anaylzer.sumOfJanuary();
